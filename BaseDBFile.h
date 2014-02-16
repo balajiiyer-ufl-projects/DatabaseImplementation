262608bcb318e55f826cd0a107a315fb5819e45c
Balaji Iyer <balajiciyer@gmail.com>
2014-02-16 12:40:32 -0500

    Revert "Test Changes in Make file"
    This reverts commit efed0b3f8349034c72c5378d5d8b9c211942d9a7.

--- BaseDBFile.h
+++ BaseDBFile.h
@@ -1,39 +1,17 @@
-#ifndef BASEDBFILE_H
-#define BASEDBFILE_H
+//
+//  BaseDBFile.h
+//  databaseproj
+//
+//  Created by Balaji Iyer on 2/14/14.
+//  Copyright (c) 2014 Balaji Iyer. All rights reserved.
+//
 
-
-
-#include "TwoWayList.h"
-#include "Record.h"
-#include "Schema.h"
-#include "File.h"
-#include "Comparison.h"
-#include "ComparisonEngine.h"
+#ifndef __databaseproj__BaseDBFile__
+#define __databaseproj__BaseDBFile__
 
 #include <iostream>
 
 
 
-class BaseDBFile{
-    
-public:
-    BaseDBFile ();
-    ~BaseDBFile();
-    
-    
-	virtual int Create (char *fpath, void *startup)=0;
-	virtual int Open (char *fpath)=0;
-	virtual int Close ()=0;
-    
-	virtual void Load (Schema &myschema, char *loadpath);
-    
-	virtual void MoveFirst ()=0;
-	virtual void Add (Record &addme)=0;
-	virtual int GetNext (Record &fetchme)=0;
-	virtual int GetNext (Record &fetchme, CNF &cnf, Record &literal)=0;
-};
 
-#endif
-
-
-
+#endif /* defined(__databaseproj__BaseDBFile__) */
