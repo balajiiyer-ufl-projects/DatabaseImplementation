b0c279a6a371197b82fbb62baf74a860a23eaf43
Sanyogita Ranade <sanyoranade@gmail.com>
2014-02-16 18:32:29 -0500

    Reverted back original DBFile.cc and DBFile.h, the one from the backup of assignment 1 submission

--- DBFile.h
+++ DBFile.h
@@ -1,8 +1,6 @@
 #ifndef DBFILE_H
 #define DBFILE_H
 
-
-
 #include "TwoWayList.h"
 #include "Record.h"
 #include "Schema.h"
@@ -10,50 +8,23 @@
 #include "Comparison.h"
 #include "ComparisonEngine.h"
 
-#include <iostream>
-
 typedef enum {heap, sorted, tree} fType;
-
-
-class BaseDBFile{
-    
-public:
-    BaseDBFile ();
-    ~BaseDBFile();
-    
-    
-	virtual int Create (char *fpath, fType file_type, void *startup)=0;
-	virtual int Open (char *fpath)=0;
-	virtual int Close ()=0;
-    
-	virtual void Load (Schema &myschema, char *loadpath);
-    
-	virtual void MoveFirst ()=0;
-	virtual void Add (Record &addme)=0;
-	virtual int GetNext (Record &fetchme)=0;
-	virtual int GetNext (Record &fetchme, CNF &cnf, Record &literal)=0;
-};
 
 
 // stub DBFile header..replace it with your own DBFile.h
 
-class DBFile  {
-
-private:
-    File file;
-    Page page;
-    bool isFileOpen;
-    int currentPageNumber;
-    int lastPageNumber;
-    char* fileName;
-    bool isPageDirty;
+class DBFile {
+	File file;
+	Page page;
+	int currentPageNumber;
+	bool isPageDirty;
     
     
 public:
+	
 	DBFile ();
-    virtual ~DBFile();
     
-    int Create (char *fpath, fType file_type, void *startup);
+    	int Create (char *fpath, fType file_type, void *startup);
 	int Open (char *fpath);
 	int Close ();
     
@@ -63,12 +34,7 @@ public:
 	void Add (Record &addme);
 	int GetNext (Record &fetchme);
 	int GetNext (Record &fetchme, CNF &cnf, Record &literal);
-    void WritePageToFileIfDirty(Page* page, int);
-    void initializePage();
-    
-    
+	void WritePageToFileIfDirty(Page* page, int);
+	void initializePage();    
 };
 #endif
-
-
-
