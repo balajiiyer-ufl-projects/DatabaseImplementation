9ee30a10c3495111d21a9568e99d7e98db342b25
Balaji Iyer <balajiciyer@gmail.com>
2014-02-17 17:12:00 -0500

    Merged conflicts

--- BigQ.cc
+++ BigQ.cc
@@ -146,11 +146,7 @@ int BigQ::MergeRuns(){
     file.GetPage(&(runArray[0].currentPage), runArray[0].currentPageNumber);
     /*Initialize Run data structure*/
     for(int i = 1; i< numOfRuns; i++){
-<<<<<<< HEAD
         runArray[i].currentPageNumber = (runIndices[i-1])*(i-1) + 1;
-=======
-        runArray[i].currentPageNumber = (runIndices[i-1]) * (i-1) + 1;
->>>>>>> 0f6933f4d0a22afda5a63d2384b1932f5944ab20
 	cout << "Initially we push " << runArray[i].currentPageNumber << " th page for run number " << i << endl;
         runArray[i].totalPages = runIndices[i];
         file.GetPage(&(runArray[i].currentPage), runArray[i].currentPageNumber);
@@ -167,40 +163,29 @@ int BigQ::MergeRuns(){
     
     //
     while(numOfRuns>0){
-	cout << "Before you pop the record from merge queue" << endl;        
+        
         //Fetch the record from priority queue
         RecordStruct *candidate=mergeQueue.top();
         //Insert each record into output queue
         outPipe->Insert(&(candidate->record));
         //Delete the record from priority queue
         mergeQueue.pop();
-	cout << "After you pop the record from merge queue" << endl;
         //Get the next record from the run
-<<<<<<< HEAD
         RecordStruct *nextCandidate=new RecordStruct;
         if(!GetNextRecordFromRun(candidate->run_num,runArray,nextCandidate))
         {
-=======
-        RecordStruct *nextCandidate;
-        nextCandidate=GetNextRecordFromRun(candidate->run_num,runArray);
-        cout<<&nextCandidate<<endl;
-        if(!nextCandidate){
-		cout << "inside if " <<endl;
->>>>>>> 0f6933f4d0a22afda5a63d2384b1932f5944ab20
             numOfRuns--;
             //Delete the run if exhausted
             delete (&runArray[candidate->run_num]);
         }
         else{
-		cout << "inside else" <<endl;
             //Add the next record to queue
             mergeQueue.push(nextCandidate);
-		 cout << "inside else after" <<endl;
         }
         
     }
     
-   cout << "While loop ends" <<endl; 
+    
     // Check if all runs exhausted, exit
     file.Close();
     delete &runIndices;
@@ -214,17 +199,11 @@ bool BigQ::GetNextRecordFromRun(int currentRunNumber,Run *array,RecordStruct *ne
     //RecordStruct *nextRecord=new RecordStruct;
     int status;
     if(array[currentRunNumber].currentPage.GetFirst(&(nextRecord->record))){
-<<<<<<< HEAD
         status=1;
-=======
-	cout <<"Regular case" << endl;
-        return nextRecord;
->>>>>>> 0f6933f4d0a22afda5a63d2384b1932f5944ab20
     
     }
     //Check for more pages in the run
     else{
-	cout << "Here 1" <<endl;
         //Decrement the number of pages in the run
 	cout << "Total pages to be consumed are "<< array[currentRunNumber].totalPages << " and current page number is " << array[currentRunNumber].currentPageNumber << " for run no " << currentRunNumber <<endl;
         array[currentRunNumber].totalPages--;
@@ -237,14 +216,10 @@ bool BigQ::GetNextRecordFromRun(int currentRunNumber,Run *array,RecordStruct *ne
             array[currentRunNumber].currentPage.GetFirst(&(nextRecord->record));
             status=1;
         }
-        else{	cout << "here 2" <<endl;
+        else{
 		cout << "Case where run no " << currentRunNumber << "got exhausted " <<endl;
-<<<<<<< HEAD
             //delete nextRecord;
             status=0;
-=======
-            delete nextRecord;
->>>>>>> 0f6933f4d0a22afda5a63d2384b1932f5944ab20
         }
     }
     
